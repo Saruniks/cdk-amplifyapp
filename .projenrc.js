e9e3fb66baa9396f3db67ddeb1aadadd516b4ac8
Sarunas Gincas <sg@nikulipe.com>
2023-03-12 20:00:12 +0200

    chore: project created with projen

--- .projenrc.js
+++ .projenrc.js
@@ -1,8 +1,8 @@
-import { typescript } from 'projen';
-const project = new typescript.TypeScriptProject({
+const { awscdk } = require('projen');
+const project = new awscdk.AwsCdkTypeScriptApp({
+  cdkVersion: '2.1.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-amplifyapp',
-  projenrcTs: true,
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
